31e2dbc9be0e3b642e1f12ed4fb0bc84db8c7286
Dasom.Kim <dahappy1026@gmail.com>
2016-11-28 04:23:29 -0800

    myshell

---
+++ myshell.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main()
+{
+	int pid, fd;
+	SYMBOL term;
+	print_prompt();
+	
+	while (TRUE)
+	{
+		term = parse(&pid, FALSE, NULL);
+		
+		if (term != S_AMP && pid != 0)
+		{
+			waitfor(pid);
+		}
+		
+		if (term == S_NL)
+		{
+			print_prompt();
+		}
+		
+		for (fd=3; fd<MAXFD; fd++)
+		{
+			close(fd);
+		}
+	}
+}
+

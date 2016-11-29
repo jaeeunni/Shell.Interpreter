e3615810084257112bc87d5e88bf608e45af7b03
jaeeunni <pitit@hanmail.net>
2016-11-29 02:33:30 -0800

    mv

--- command.c
+++ command.c
@@ -151,3 +151,22 @@ int cmd_cat(int argc,char **argv)
 }
 
 
+int cmd_mv(int argc, char*argv[])
+{
+	int fd1,fd2;
+	int r_size,w_size;
+	char buf[100];
+
+	fd1=open(argv[1],O_RDONLY);
+	fd2=open(argv[2],O_RDWR|O_CREAT|O_EXCL,0664);
+
+	r_size=read(fd1,buf,100);
+	w_size=write(fd2,buf,r_size);
+	while(r_size==100)
+	{
+		r_size=read(fd1,buf,100);
+		w_size=write(fd2,buf,r_size);
+	}
+	unlink(argv[1]);
+	return 0;
+}

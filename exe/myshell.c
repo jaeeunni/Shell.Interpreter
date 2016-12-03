6dabc6a020210884968df288e7a8869b71f7ab08
Dasom.Kim <dahappy1026@gmail.com>
2016-12-02 23:15:56 -0800

    direxe

---
+++ exe/myshell.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "common.h"
+
+int main()
+{
+	int pid, fd;
+	SYMBOL term;
+	print_prompt();
+	
+	while (TRUE)
+	{
+		term = parse(&pid, FALSE, NULL);
+		
+		if (term != S_AMP && pid != 0)
+		{
+			waitfor(pid);
+		}
+		
+		if (term == S_NL)
+		{
+			print_prompt();
+		}
+		
+		for (fd=3; fd<MAXFD; fd++)
+		{
+			close(fd);
+		}
+	}
+}
+
